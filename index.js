7cd9b794af8f194f0bb41f1830ec4ecbf389acf5
Andrei Korchagin <andrei.korchagin@gmail.com>
2018-01-26 20:54:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,29 +2,38 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendKitten(name){
-  
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
 }
 
-function destructivelyPrependKitten(name){
-  
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
 }
 
-function destructivelyRemoveLastKitten(){
-  
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
 }
 
-function appendKitten(name){
-  
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
 }
 
-function prependKitten(name){
-  
+function appendKitten(name) {
+  return [...kittens, name];
 }
 
-function removeLastKitten(){
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
   return kittens.slice(kittens.length() - 1, kittens.length());
 }
-function removeFirstKitten(){
-  return kittens.slice(0,1);
+
+function removeFirstKitten() {
+  return kittens.slice(0, 1);
 }
