0462bc135b29fa8773847df4a4ad156c3b20507a
Andrei Korchagin <andrei.korchagin@gmail.com>
2018-01-26 20:45:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  
+}
+
+function destructivelyPrependKitten(name){
+  
+}
+
+function destructivelyRemoveLastKitten(){
+  
+}
+
+function appendKitten(name){
+  
+}
+
+function prependKitten(name){
+  
+}
+
+function removeLastKitten(){
+  return kittens.slice(kittens.length() - 1, kittens.length());
+}
+function removeFirstKitten(){
+  return kittens.slice(0,1);
+}
